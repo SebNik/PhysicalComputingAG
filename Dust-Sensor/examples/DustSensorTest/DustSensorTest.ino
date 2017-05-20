a57fd28420b978f502461a1710ef626bdf8bd7b4
f41ardu <f41thr@arcor.de>
2017-05-20 07:23:23 +0200

    Update Sensor Info
    Pins für den Staubsensor und Dokumentation

--- Dust-Sensor/examples/DustSensorTest/DustSensorTest.ino
+++ Dust-Sensor/examples/DustSensorTest/DustSensorTest.ino
@@ -2,10 +2,19 @@
   Standalone Sketch to use with a Arduino Duemilanove, Uno, etc....  and a
   Sharp Optical Dust Sensor GP2Y1010AU0F
   06.11.2016 #f41_ardu 
+  
+  
+1  V-LED) 	3.3V Pin (150 Ohm in between)
+2 (LED-GND) 	GND Pin
+3 (LED) 	Digital Pin 12
+4 (S-GND) 	GND Pin
+5 (Vo) 	Analog Pin A6
+6 (Vcc) 	3.3V Pin (Direct)
+  
 */
 #include "DustSensor.h"
 
-int measurePin = 0; //Connect dust sensor to Arduino A0 pin
+int measurePin = 6; //Connect dust sensor to Arduino A6 pin
 int ledPower = 2;   //Connect 3 led driver pins of dust sensor to Arduino D2
 
 DustSensor GP2Y1010AU0F(ledPower, measurePin);
